8858c6d118303dcd66cbdaa96d8a580060090ffb
Pedro Brandão <redpig825@gmail.com>
2026-03-12 09:18:19 -0300

    Change background color in Home container styles

--- src/Containers/Home/styles.js
+++ src/Containers/Home/styles.js
@@ -4,8 +4,9 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
+    height: 100vh;
     display: flex;
-    background-color: #cbebb5;
+    background-color: #efd3f0;
     flex-direction: column;
     justify-content: center;
     gap: 2rem;
