69e7b8995b619502c588befef206c5d91765d6c5
Pedro Brandão <redpig825@gmail.com>
2026-03-14 00:38:49 -0300

    Fix link path for 'Faça Sua Encomenda'

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -16,7 +16,7 @@ function Home() {
             <LinksBox className="links">
                 <Link target="_blank" rel="noopener noreferrer" href="./cardapio.pdf">Cardápio de Páscoa</Link>
                 <Link target="_blank" rel="noopener noreferrer" href="https://w.app/lacacau_confeitaria">Atendimento via Whatsapp</Link>
-                <Link target="_blank" rel="noopener noreferrer" href="./public/cardapio/index.html">Faça Sua Encomenda</Link>
+                <Link target="_blank" rel="noopener noreferrer" href="./cardapio/index.html">Faça Sua Encomenda</Link>
                 <Link target="_blank" rel="noopener noreferrer" href="https://qro.go.link/enjrM">Quero Delivery</Link>
             </LinksBox>
         </Container>
