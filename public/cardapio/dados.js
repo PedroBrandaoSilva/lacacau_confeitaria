7b4f53c885002f74395378f4ea026979c64341df
PedroBrandaoSilva <redpig825@gmail.com>
2026-06-08 17:21:40 -0300

    Menu updated

---
+++ public/cardapio/dados.js
@@ -0,0 +1,81 @@
+export const menu = [
+    {
+        categoria: "Tortas",
+        subtitulo: "(QUALQUER FATIA 17,00)",
+        itens: [
+            { nome: "CHOCOLATUDA", preco: "17,00", imagem: { src: "img3.jpeg" } },
+            { nome: "FERRERO ROCHER", preco: "17,00", imagem: { src: "img22.jpeg" } },
+            { nome: "NINHO COM NUTELLA", preco: "17,00", imagem: { src: "img24.jpeg" } },
+            { nome: "TENTAÇÃO", preco: "17,00", imagem: { src: "img21.jpeg" } },
+            { nome: "SUPREMA", preco: "17,00", imagem: { src: "img27.jpg" } },
+            { nome: "OLHO DE SOGRA", preco: "17,00", imagem: { src: "img14.jpeg" } },
+            { nome: "REDVELVET", preco: "17,00", imagem: { src: "img26.jpeg" } },
+            { nome: "ESPERANÇA", preco: "17,00", imagem: { src: "img15.jpeg" } },
+            { nome: "MATILDA", preco: "50,00", imagem: { src: "img4.jpeg" } },
+            { nome: "CHEESECAKE DE OREO", preco: "17,00", imagem: { src: "img1.jpeg" } },
+            { nome: "CHEESECAKE DE MORANGO", preco: "17,00", imagem: { src: "img9.jpeg" } },
+            { nome: "CHEESECAKE DE LIMÃO", preco: "17,00", imagem: { src: "img2.jpeg" } }
+        ]
+    },
+
+    {
+        categoria: "Doces e Sobremesas",
+        itens: [
+            { nome: "PASTELZINHO DE NINHO", preco: "6,00", imagem: { src: "img10.jpeg" } },
+            { nome: "DOCINHO", preco: "2,00", imagem: { src: "img23.jpeg" } },
+            { nome: "TARTELET", preco: "8,00", imagem: { src: "img25.jpeg" } },
+
+            {
+                nome: "DOSE DA FELICIDADE",
+                preco: "20,00",
+                imagem: { src: "img41.jpeg" },
+                descricao:
+                    "Morango, Ferrero, Ninho, Sonho de Valsa e Ouro Branco"
+            },
+
+            { nome: "PAVÊ", preco: "8,00", imagem: { src: "img18.jpeg" } },
+            { nome: "PUDIM", preco: "7,00", imagem: { src: "img32.jpeg" } },
+            { nome: "BROWNIE RECHEADO", preco: "8,00", imagem: { src: "img20.jpeg" } },
+            { nome: "CESTINHA GOURMET", preco: "17,00", imagem: { src: "img36.jpeg" } },
+            { nome: "COPINHO RECHEADO", preco: "9,00", imagem: { src: "img8.jpeg" } },
+            { nome: "CONE TRUFADO", preco: "10,00 / 12,00", imagem: { src: "img19.jpeg" } },
+            { nome: "TRUFA", preco: "5,00", imagem: { src: "img43.jpg" } }
+        ]
+    },
+
+    {
+        categoria: "Salgados",
+        itens: [
+            { nome: "TORTA DE FRANGO", preco: "10,00", imagem: { src: "img17.jpeg" } },
+            { nome: "EMPADÃO", preco: "10,00", imagem: { src: "img12.jpeg" } },
+            { nome: "PASTEL DE FORNO", preco: "8,00", imagem: { src: "img6.jpeg" } },
+            { nome: "ESFIHA CARNE", preco: "8,00", imagem: { src: "img38.jpg" } },
+            { nome: "ESFIHA FRANGO", preco: "8,00", imagem: { src: "img35.jpeg" } },
+            { nome: "MISTO", preco: "8,00", imagem: { src: "img11.jpeg" } },
+            { nome: "JOELHO", preco: "8,00", imagem: { src: "img5.jpeg" } },
+            { nome: "CONE RECHEADO", preco: "12,00", imagem: { src: "img34.jpeg" } }
+        ]
+    },
+
+    {
+        categoria: "Bolos",
+        itens: [
+            { nome: "BOLO VULCÃO 400G", preco: "22,00", imagem: { src: "img16.jpeg" } },
+            { nome: "BOLO VULCÃO 800G", preco: "40,00", imagem: { src: "img40.jpeg" } },
+            { nome: "ESCONDIDINHO DE BROWNIE", preco: "10,00", imagem: { src: "img7.jpeg" } },
+            { nome: "TORTA NO POTE", preco: "12,00", imagem: { src: "img13.jpeg" } }
+        ]
+    },
+
+    {
+        categoria: "Bebidas",
+        itens: [
+            { nome: "REFRIGERANTE EM LATA", preco: "6,00", imagem: { src: "img30.jpg" } },
+            { nome: "COCA PET", preco: "3,50", imagem: { src: "img28.jpg" } },
+            { nome: "SUCO DELVALLE", preco: "5,50", imagem: { src: "img29.jpg" } },
+            { nome: "COCA 1L", preco: "9,00", imagem: { src: "img31.jpg" } },
+            { nome: "CHOCOLATE QUENTE", preco: "15,00", imagem: { src: "img39.jpeg" } },
+            { nome: "CAPPUCCINO", preco: "12,00", imagem: { src: "img39.jpeg" } }
+        ]
+    }
+];
